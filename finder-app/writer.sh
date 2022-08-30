735f2cd3e230ce0291a20b6793b9d1b2cbd95a61
swapnilghonge <swapnil.ghonge@gmail.com>
2022-08-29 22:11:24 -0600

    Final Commit

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -3,6 +3,9 @@
 # Author: Swapnil Ghonge
 # Description: This file contains functon of writer.sh
 
+# References: https://www.youtube.com/watch?v=V1y-mbWM3B8
+#		https://linuxconfig.org/bash-scripting-tutorial-for-beginners
+#		https://linux.die.net/man/1/mkdir		
 
 # Error Checking for number of arguements
 if [ $# -ne 2 ]
@@ -11,6 +14,7 @@ then
 	
 	exit 1
 fi
+
 
 echo $2>$1
 
