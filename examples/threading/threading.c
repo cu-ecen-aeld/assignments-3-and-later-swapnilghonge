0d5e77d5146eef7f3e3cdd16b2370efbf8fa4fa2
swapnilghonge <swapnil.ghonge@gmail.com>
2022-09-17 14:11:43 -0600

    Adding the Threading Assignment 4 Part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,16 +10,34 @@
 
 void* threadfunc(void* thread_param)
 {
-
+	
+	
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    //Wait 
+    usleep(1000*thread_func_args->wait_to_obtain_ms);
+    
+    //locking the mutex
+    pthread_mutex_lock(thread_func_args->mutex);
+    
+    //wait
+    usleep(1000*thread_func_args->wait_to_release_ms);
+    
+    // release the mutex
+    pthread_mutex_unlock(thread_func_args->mutex);
+    
+    // thread as success
+    thread_func_args->thread_complete_success = true;
+    
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
+    	
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
@@ -28,6 +46,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+     
+     struct thread_data *thread_params;
+     
+     // allocate the memory
+     thread_params = malloc (sizeof(struct thread_data));
+     
+     // wair arguments
+     thread_params->wait_to_obtain_ms = wait_to_obtain_ms;
+     
+     thread_params->wait_to_release_ms = wait_to_release_ms;
+     
+     //thread data
+     thread_params->mutex = mutex;
+     
+     // pass all thread data to created thread.
+     if (pthread_create(thread, 0, threadfunc, (void *)thread_params) != 0)
+        
+        {
+        return false;
+        }
+        
+    return true;
+     
 }
 
