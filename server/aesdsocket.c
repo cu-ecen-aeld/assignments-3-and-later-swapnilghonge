437973390e0626039c7c5f176284a0076656f0c7
swapnilghonge <swapnil.ghonge@gmail.com>
2022-10-02 18:08:40 -0600

    full test running locally

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,314 +1,241 @@
-/* Assignment -5 part1 
- * Author : Swapnil Ghonge
- * Details: This files contains fucntions which coorelate with aesdsocket.c.
- */
+/* Author: Swapnil Ghonge
+ * Assignment: Assignment 5 Part1 
+ * Refernces: 	https://www.geeksforgeeks.org/socket-programming-cc/
+ *		https://beej.us/guide/bgnet/html/#sockaddr_inman
+ *		https://beej.us/guide/bgnet/html/#sockaddr_inman
+ *		https://www.csd.uoc.gr/~hy556/material/tutorials/cs556-3rd-tutorial.pdf
+ *		https://man7.org/linux/man-pages/man2/bind.2.html
+*/
+
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
+#include <errno.h>
 #include <netinet/in.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
-#include <ctype.h>
-#include <syslog.h>
-#include <unistd.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <syslog.h>
+#include <net/if.h>
+
+#define MYPORT 9000
+#define BUF_SIZE 1024
 
 
-#define TOTAL_MSG	3	
-#define ERROR		1
-#define SUCCESS		0
-#define PORT		9000
+int file_fd;
+int sockfd;
+int yes=1;
 
-
-int sockfd = 0;
-char *sendbuffer = NULL;
-int acceptfd;
-int fd = 0;
-sigset_t mask;
-
-//filename and path in one variable
-const char *filepath = "/var/tmp/aesdsocketdata";
-
- void closeall(void)
+void signal_hanlder(int signum)
 {
-	if(sendbuffer != NULL)	
-		free(sendbuffer);
-
-	if(acceptfd)
-		close(accept);
-
-	if(sockfd)
-		close(sockfd);
-
-	if(fd)	
-	{
-		close(fd);
-		remove(filepath);
-	}
-	
-	closelog();	
+    printf("Caught signal %d", signum);
+    if ((signum == SIGINT) || (signum == SIGTERM)) {
+        close(file_fd);
+        close(sockfd);
+        remove("/var/tmp/aesdsocketdata");
+        exit(0);
+    }
 }
 
-void signal_handler(int signo)
+int main(int argc, char **argv)
 {
 
-	syslog(LOG_DEBUG, "in handler\n");
-	
-	if(signo == SIGINT)
-		syslog(LOG_DEBUG, "Caught signal SIGINT, exiting\n");
-	else
-		syslog(LOG_DEBUG, "Caught signal SIGTERM, exiting\n");
-
-	closeall();
-
-	exit(EXIT_SUCCESS);
-	
-}
-
-
-
-
-int main(int argc, char *argv[])
-{
-	openlog(NULL, LOG_CONS, LOG_USER);
-
-	struct sockaddr_in saddr;
-	char buffer[50] = {0};
-	int nbytes = 0;
-	ssize_t nr = 0;
-	unsigned int total_bytes = 0;
-	off_t location =0;
-	int daemon=0;
-
-	
-    if ((argc == 2) && (strcmp("-d", argv[1])==0)) 
-		daemon = 1;
-
-	
-	if(signal(SIGINT, signal_handler) == SIG_ERR)
-	{
-		syslog(LOG_ERR, "Cannot handle SIGINT!\n");
-		exit(EXIT_FAILURE);
-	}
-
-	
-	if(signal(SIGTERM, signal_handler) == SIG_ERR)
-	{
-		syslog(LOG_ERR, "Cannot handle SIGTERM!\n");
-		exit(EXIT_FAILURE);
-	}
-
-	
-    if (sigemptyset(&mask) == -1) 
-	{
-        syslog(LOG_ERR, "creating empty signal set failed");
-        exit(EXIT_FAILURE);
+    signal(SIGINT, signal_hanlder);
+    signal(SIGTERM, signal_hanlder);
+    int rec;
+    struct sockaddr_in servadd, clientadd;
+    socklen_t client_len;
+    int clientfd;
+    
+    //Calling the socket function to establish connection
+   
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        perror("Error in socket\n");
+        return -1;
     }
-	
-    if (sigaddset(&mask, SIGINT) == -1) 
-	{
-        syslog(LOG_ERR, "Adding SIGINT failed");
-        exit(EXIT_FAILURE);
-    }
-	
-    if (sigaddset(&mask, SIGTERM) == -1) 
-	{
-        syslog(LOG_ERR, "Adding SIGTERM failed");
-        exit(EXIT_FAILURE);
+    
+    
+    memset((void *)&servadd, 0, sizeof(servadd));
+    servadd.sin_family = AF_INET;
+    servadd.sin_port = htons(MYPORT);
+    servadd.sin_addr.s_addr = INADDR_ANY;
+
+    
+    if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) 
+    {
+    	perror("setsockopt error");
+    	exit(1);
     }
 
 
+    
+    int sock = bind(sockfd, (struct sockaddr *)&servadd, sizeof(struct sockaddr));
+    if (sock == -1)
+    {
+        close(sockfd);
+        perror("Error binding \n");
+        return -1;
+    }
 
-	// setting the arguements for socket
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1)
-	{
-		syslog(LOG_ERR, "Error in socket\n");
-		exit(EXIT_FAILURE);
-	}
+    if(argc > 1)
+    {
+        if(!strcmp(argv[1],"-d"))
+        {
+            
+            /*create a process*/
+            pid_t pid = fork();
 
-	saddr.sin_family = AF_INET;
-	saddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	saddr.sin_port = htons(PORT);
+    		if (pid < 0) 
+    		{
+    			
+        		exit (-1);
+    		}
+    		else if (pid == 0)
+    		{
+    			/*create a new session and prcess group*/
+    			if (setsid() < 0) 
+    			{
+        			
+        			exit (-1);
+        		}
+    			/*set the working directory to the root directory*/
+    			if(chdir("/") == -1)
+    			{
+    				perror("error changing directory");
+    				exit (-1);
+    			} 
+    			/*redirect fd's 0,1,2 to /dev/null*/
+    			open("/dev/null", O_RDWR); /*stdin*/
+    			dup(0);				/*stdout*/
+    			dup(0);
+    			
+    			printf("Daemon created\n");
+    			
+    		}
+            else 
+            {
+                exit (0);
+            }
+        }
+    }
 
-	// calling bind function
-	int bindstatus = bind(sockfd, (struct sockaddr *) &saddr, sizeof saddr);
-	if (bindstatus == -1)
-	{
-		syslog(LOG_ERR, "socket binding failed\n");
-		closeall();
-		exit(EXIT_FAILURE);
-	}
-	
-	// calling listen function
-	bindstatus = listen(sockfd, 0);
-	if (bindstatus == -1)
-	{
-		syslog(LOG_ERR, "socket listening failed\n");
-		closeall();
-		exit(EXIT_FAILURE);
-	}
-	
-	socklen_t len = sizeof(struct sockaddr);
+    
+    int listenfd;
+    listenfd = listen(sockfd, 5);
+    printf("Listening successful\n");
 
-	//create a file on the file
-	fd = open(filepath, O_CREAT | O_RDWR | O_APPEND | O_TRUNC, 0777);
-	
-	if (fd == -1)	
-	{
-		syslog(LOG_ERR, "can't open or create file '%s'\n", filepath);
-		closeall();
-		exit(EXIT_FAILURE);
-	}
-	
-	syslog(LOG_DEBUG, "opened or created file '%s' successfully\n", filepath);
+    if (listenfd == -1)
+    {
+        perror("Error in Listening process \n");
+        return -1;
+    }
 
-	
-	
-	// start daemon
-	if (daemon) 
-	{
-        	pid_t pid = fork();
-        	if (pid == -1) 
-		{
-            		syslog(LOG_ERR, "Error in creating a child process");
-			closeall();
-            		exit(EXIT_FAILURE);
-        	}
+    
+    file_fd = open("/var/tmp/aesdsocketdata",O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP |S_IWGRP | S_IROTH | S_IWOTH, 0777);
+    
+    if (file_fd == -1)
+    {
+        perror("Error in opening file descriptor.");
+    }
+    
+    int file_size = 0;
+    
+    char receive_buffer[BUF_SIZE];
+    
+    while(1)
+    {
+        client_len = sizeof(clientadd);
+        clientfd = accept(sockfd, (struct sockaddr *)&clientadd, &client_len);
+        if (clientfd == -1)
+        {
+            perror("error accepting\n");
+            return -1;
+        }
+        printf("Accepted connection from %s\n",inet_ntoa(clientadd.sin_addr));
+        syslog(LOG_DEBUG,"Accepted connection from %s\n",inet_ntoa(clientadd.sin_addr));  
+
+        bool status = false;
+        int buffer = BUF_SIZE;
+        int buffer_size = 0;
+        char *write_buf = malloc(sizeof(char) * buffer);
+
+        while(!status)
+        {
+            // receive the data from client
+            rec = recv(clientfd, receive_buffer, BUF_SIZE, 0);
+            if (rec == 0)
+            {
+                status = true;
+            } 
+            for (int i = 0; i < rec; i++)
+            {
+                
+                // appneding new line
+ 		if (receive_buffer[i] == '\n')
+                {
+                    status = true;
+                }
+            }
+            if ((buffer - buffer_size) < rec)
+            {
+                buffer = buffer +rec;
+                write_buf = (char *) realloc(write_buf, sizeof(char) * buffer);
+            }
+            memcpy(write_buf + buffer_size, receive_buffer, rec);
+            buffer_size += rec;
+        }
         
-        	else if (pid != 0)
-		{
-            	exit(EXIT_SUCCESS);
-        	}
-		/*create a new session and process group*/	
-		if (setsid() == -1) 
-		{
-			syslog(LOG_ERR, "cannot create a new session");
-			closeall();
-			exit(EXIT_FAILURE);
-		}
-		/*set the working directory to the root directory*/
-		if (chdir("/") == -1) 
-		{
-			syslog(LOG_ERR, "cannot change directory");
-			closeall();
-			exit(EXIT_FAILURE);
-		}
-		/*redirect fd's 0,1,2 to dev/null*/
-		open("/dev/null", O_RDWR);
-		dup(0);	/*stdin*/
-		dup(0); /*stdout*/
-		dup(0);	/*stderror*/
+        if(status)
+        {
+            status = false;
+          // writing bytes to file
+            int write_bytes = write(file_fd, write_buf, buffer_size);
+            if (write_bytes == -1)
+            {
+                perror("error writing to file");
+                return -1;
+            }
+            file_size = file_size+ write_bytes;
 
-		close(STDIN_FILENO);
-		close(STDOUT_FILENO);
-		close(STDERR_FILENO);
-   	}
-	
-	while(1)
-	{
-		/*Accept command*/
-		
-		acceptfd = accept(sockfd, (struct sockaddr *) &saddr, &len);
-		if (acceptfd == -1)
-		{
-			syslog(LOG_ERR, "socket accepting failed\n");
-			closeall();
-			exit(EXIT_FAILURE);
-		}
+            lseek(file_fd, 0, SEEK_SET);
 
-		syslog(LOG_DEBUG, "Accepted connection from '%s'\n", inet_ntoa((struct in_addr)saddr.sin_addr));
-	
+            char *read_buf = (char *) malloc(sizeof(char) * file_size);
 
-		//Signals from set are added to invoking process's signal mask
-        	if (sigprocmask(SIG_BLOCK, &mask, NULL)) 
-		{
-            syslog(LOG_ERR, "sigprocmask");
-			closeall();
-            exit(EXIT_FAILURE);
+           // reading bytes
+            ssize_t read_bytes = read(file_fd, read_buf, file_size);
+            if (read_bytes == -1)
+            {
+                perror("Error in read of file");
+                return -1;
+            }
+		// send bytes
+            int send_bytes = send(clientfd, read_buf, read_bytes, 0);
+            if (send_bytes == -1)
+            {
+                perror("Error in sending data to client");
+                return -1;
+            }
+
+            // freeing memory in after using the buffer.
+            free(read_buf);
+            free(write_buf);
+
         }
-
-		do
-		{
-			memset(buffer, 0, sizeof(buffer));
-			
-			nbytes = recv(acceptfd, buffer, sizeof(buffer), 0);
-
-			if (nbytes)
-			{
-				
-				if(nbytes > strlen(buffer))
-					nr = write(fd, buffer, strlen(buffer));
-				else
-					nr = write(fd, buffer, nbytes);
-					
-				if (nr == -1)	
-				{//if error
-					syslog(LOG_ERR, "can't write received string in file '%s'\n", filepath);
-					break;	
-				}
-				total_bytes += nr;
-			}
-			else
-				break;
-
-		}while(strchr(buffer, '\n') == NULL);
-		
-				
-		nbytes=0;
-		lseek(fd, 0, SEEK_SET);
-		
-		sendbuffer = (char *)malloc(sizeof(char) * (total_bytes+location));
-		if (sendbuffer == NULL)
-			syslog(LOG_ERR, "malloc failed\n");
-		
-		memset(sendbuffer, 0, sizeof(sendbuffer));
-		nr = read(fd, sendbuffer, total_bytes+location);
-		if (nr != total_bytes+location)	
-		{//if error
-			syslog(LOG_ERR, "can't read proper bytes from file '%s'\n", filepath);
-			break;	
-		}
-		
-		//syslog(LOG_DEBUG, "'%s'\n", sendbuffer);
-		nbytes = send(acceptfd, sendbuffer, nr, 0);
-		if(nbytes != nr)
-		{
-			syslog(LOG_ERR, "not all bytes sent\n");
-			break;
-		}
-		
-		total_bytes = 0;
-		
-		location=lseek(fd, 0, SEEK_END);
-
-		syslog(LOG_DEBUG, "Closed connection from '%s'\n", inet_ntoa((struct in_addr)saddr.sin_addr));
-
-
-		//Signals from set are added to invoking process's signal mask
-        if (sigprocmask(SIG_UNBLOCK, &mask, NULL)) 
-		{
-            syslog(LOG_ERR, "sigprocmask");
-			closeall();
-            exit(EXIT_FAILURE);
-        }
-	
-		free(sendbuffer);
-		close(acceptfd);
-	}
-
-	remove(filepath);
-	close(sockfd);
-	close(fd);
-	
-	closelog();
-	return 0;
+        close(clientfd);
+        syslog(LOG_DEBUG,"Closed connection from %s\n",inet_ntoa(clientadd.sin_addr));  
+    }
+    
+    close(sock);
+    return 0;
 }
-
 
 
